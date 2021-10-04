4f002dbae20cea85b99a47b9e4287c0c00c9a430
gfy <502270682@qq.com>
2021-10-04 23:04:30 +0800

    FEAT: string format

---
+++ problem/string/482.go
@@ -0,0 +1,49 @@
+package string
+
+import (
+	"unicode"
+)
+
+/*
+一个密钥字符串 S ，只包含字母，数字以及 '-'（破折号）。其中， N 个 '-' 将字符串分成了 N+1 组。
+
+给你一个数字 K，请你重新格式化字符串，使每个分组恰好包含 K 个字符。特别地，第一个分组包含的字符个数必须小于等于 K，但至少要包含 1 个字符。
+两个分组之间需要用 '-'（破折号）隔开，并且将所有的小写字母转换为大写字母。
+
+给定非空字符串 S 和数字 K，按照上面描述的规则进行格式化。
+
+
+
+示例 1：
+
+输入：S = "5F3Z-2e-9-w", K = 4
+输出："5F3Z-2E9W"
+解释：字符串 S 被分成了两个部分，每部分 4 个字符；
+     注意，两个额外的破折号需要删掉。
+
+*/
+
+func licenseKeyFormatting(s string, k int) string {
+	count := 0
+	// 97~122： a~z
+	ans := []byte{}
+	length := len(s)
+	for i := length - 1; i >= 0; i-- {
+		// 直接跳过没有意义的"-"
+		if s[i] != '-' {
+			ans = append(ans, byte(unicode.ToUpper(rune(s[i]))))
+			count++
+			if count%k == 0 {
+				ans = append(ans, '-')
+			}
+		}
+	}
+	if len(ans) > 0 && ans[len(ans)-1] == '-' {
+		ans = ans[:len(ans)-1]
+	}
+	// 翻转
+	for i, n := 0, len(ans); i < n/2; i++ {
+		ans[i], ans[n-i-1] = ans[n-i-1], ans[i]
+	}
+	return string(ans)
+}
