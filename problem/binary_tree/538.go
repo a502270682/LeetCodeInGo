17358a4d6f6c49dcca65b9da9ccaaf101eed4d06
feiyangguo <feiyang.guo@iglooinsure.com>
2022-02-17 11:54:38 +0800

    mid: 540, erfen

--- problem/binary_tree/538.go
+++ problem/binary_tree/538.go
@@ -1,14 +1,14 @@
 package binary_tree
 
 /*
-给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
+给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node的新值等于原树中大于或等于node.val 的值之和。
 
 提醒一下，二叉搜索树满足下列约束条件：
 
 节点的左子树仅包含键 小于 节点键的节点。
 节点的右子树仅包含键 大于 节点键的节点。
 左右子树也必须是二叉搜索树。
-注意：本题和 1038: https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/ 相同
+注意：本题和 1038:https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/ 相同
 
 
 
