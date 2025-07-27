01ce441874b959dee7a149e061e5c5ca5b3cda48
gfy <502270682@qq.com>
2025-07-27 22:56:31 +0800

    erfen: 34

--- 2025/er_fen/74.go
+++ 2025/er_fen/74.go
@@ -2,7 +2,10 @@ package er_fen
 
 func searchMatrix(matrix [][]int, target int) bool {
 	m, n := len(matrix), len(matrix[0])
-	left, right := 0, m*n
+	if m == 0 || n == 0 {
+		return false
+	}
+	left, right := 0, m*n-1
 	for left <= right {
 		mid := left + (right-left)/2
 		if matrix[mid/n][mid%n] == target {
